10dc43ef63bf1790927340d28a5292835e09da1f
Cathy Dao <cathydao95@gmail.com>
2023-10-23 09:26:00 -0700

    connect to recipes db

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -3,9 +3,6 @@ const { Pool } = pkg;
 
 const db = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: process.env.DATABASE_SSL !== "false" && {
-    rejectUnauthorized: false,
-  },
 });
 
 export default db;
