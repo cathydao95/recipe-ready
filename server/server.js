e0ceed68443ed7e1f0835f59331baa0718c49981
Cathy Dao <cathydao95@gmail.com>
2023-10-23 10:33:55 -0700

    create error handling middleware and custom errors

--- server/server.js
+++ server/server.js
@@ -3,17 +3,20 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import "dotenv/config";
 import morgan from "morgan";
-import cookieParser from "cookie-parser";
+import { body, validationResult } from "express-validator";
 
 // custom imports
 // routers
 import recipeRouter from "./routers/recipes.js";
 
+// middleware
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Configuring cors middleware
 app.use(cors());
-
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -25,11 +28,7 @@ app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
 });
 
-// error middleware (500 - occurs when there is an error durng the processing of a request)
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: "something went wrong" });
-});
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () =>
   console.log(`Server running on Port http://localhost:${PORT}`)
