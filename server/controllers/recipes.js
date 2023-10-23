d0e50e4566b5311c8e627565ddf917888dc464c9
Cathy Dao <cathydao95@gmail.com>
2023-10-23 11:01:01 -0700

    add get user's recipe and get nutrition

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -2,7 +2,6 @@ import db from "../db/db-connection.js";
 import "dotenv/config";
 import { StatusCodes } from "http-status-codes";
 import { NotFoundError } from "../errors/customErrors.js";
-
 export const getRecipes = async (req, res) => {
   const { rows: recipes } = await db.query("SELECT * FROM recipes");
   res.status(StatusCodes.OK).json({
@@ -11,95 +10,125 @@ export const getRecipes = async (req, res) => {
     data: { recipes },
   });
 };
-
 export const getRecipe = async (req, res) => {
   const { id } = req.params;
-
   const { rows: recipe } = await db.query("SELECT * FROM recipes WHERE id=$1", [
     id,
   ]);
-
   if (recipe.length === 0) throw new NotFoundError(`no recipe with id ${id}`);
-
   res.status(StatusCodes.OK).json({
     status: "success",
     data: { recipe },
   });
 };
 
-export const createRecipe = async (req, res) => {
-  const {
-    title,
-    ingredients,
-    instructions,
-    prep__time,
-    image_url,
-    public_recipe,
-  } = req.body;
-
-  // testing public (figure out how to default public)
-  const { rows: newRecipe } = await db.query(
-    "INSERT INTO recipes (title, ingredients, instructions, prep_time, image_url, public) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-    [title, ingredients, instructions, prep__time, image_url, public_recipe]
+export const getUsersRecipes = async (req, res) => {
+  const { userId } = req.user;
+  const { rows: recipes } = await db.query(
+    "SELECT * FROM recipes WHERE user_id = $1",
+    [userId]
   );
-
-  // private
-  // const { rows: newRecipe } = await db.query(
-  //   "INSERT INTO recipes (title, ingredients, instructions, prep_time, image_url, user_id, public) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-  //   []
-  // );
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    results: recipes.length,
+    data: { recipes },
+  });
+};
+export const createRecipe = async (req, res) => {
+  const { userId } = req.user;
+  console.log(userId);
+  const { title, ingredients, instructions, prep_time, image_url } = req.body;
+  const user_id = userId ? userId : null;
+  const public_recipe = user_id !== null && false;
+  console.log(user_id, public_recipe);
+  const { rows: newRecipe } = await db.query(
+    "INSERT INTO recipes (title, ingredients, instructions, prep_time, image_url, user_id, public_recipe) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+    [
+      title,
+      ingredients,
+      instructions,
+      prep_time,
+      image_url,
+      user_id,
+      public_recipe,
+    ]
+  );
   res.status(StatusCodes.CREATED).json({
     status: "success",
     data: { newRecipe },
   });
 };
-
 export const editRecipe = async (req, res) => {
   const { id } = req.params;
-  const {
-    title,
-    ingredients,
-    instructions,
-    prep__time,
-    image_url,
-    public_recipe,
-  } = req.body;
+  const { userId } = req.user;
+  const { title, ingredients, instructions, prep_time, image_url } = req.body;
+
+  const { rows: existingRecipe } = await db.query(
+    "SELECT * FROM recipes WHERE id = $1",
+    [id]
+  );
+  if (existingRecipe.length === 0)
+    throw new NotFoundError(`no recipe with id ${id}`);
+  const ownerId = existingRecipe[0].user_id;
+  const currentUserId = userId;
+  if (ownerId !== currentUserId) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      status: "error",
+      message: "You are not authorized to edit this recipe.",
+    });
+  }
 
   const { rows: updatedRecipe } = await db.query(
-    "UPDATE recipes SET (title, ingredients, instructions, prep_time, image_url, public) = ($1, $2, $3, $4, $5, $6) WHERE id = $7 RETURNING *",
-    [title, ingredients, instructions, prep__time, image_url, public_recipe, id]
+    "UPDATE recipes SET (title, ingredients, instructions, prep_time, image_url) = ($1, $2, $3, $4, $5) WHERE id = $6 RETURNING *",
+    [title, ingredients, instructions, prep_time, image_url, id]
   );
 
-  if (updatedRecipe.length === 0)
-    throw new NotFoundError(`no recipe with id ${id}`);
+  if (updatedRecipe.length === 0) {
+    throw new NotFoundError(`No recipe with id ${id}`);
+  }
   res.status(StatusCodes.OK).json({
     status: "success",
     data: { updatedRecipe },
   });
 };
-
 export const deleteRecipe = async (req, res) => {
   const { id } = req.params;
+  const { userId } = req.user;
+  const { rows: existingRecipe } = await db.query(
+    "SELECT * FROM recipes WHERE id = $1",
+    [id]
+  );
+  if (existingRecipe.length === 0)
+    throw new NotFoundError(`no recipe with id ${id}`);
+  const ownerId = existingRecipe[0].user_id;
+  const currentUserId = userId;
+  if (ownerId !== currentUserId) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      status: "error",
+      message: "You are not authorized to delete this recipe.",
+    });
+  }
   const { rows: deletedRecipe } = await db.query(
     "DELETE FROM recipes WHERE id = $1",
     [id]
   );
-  console.log(deletedRecipe);
-  if (deletedRecipe.length === 0)
-    throw new NotFoundError(`no recipe with id ${id}`);
   res.status(StatusCodes.OK).json({
     status: "success",
   });
 };
 
 export const getRecipeNutrition = async (req, res) => {
-  // send back ID and use ID to find recipe and grab title
   // LOOK INTO URL ENCODED
-  // const { title } = req.query;
-  // let queryTitle = title.replace(/ /g, "+");
+  // const { id } = req.params;
+  // console.log(id);
+  // const { rows: recipe } = await db.query("SELECT * FROM recipes WHERE id=$1", [
+  //   id,
+  // ]);
+  // let title = recipe[0].title;
+  // title = title.replace(/ /g, "+");
   // try {
   //   const response = await fetch(
-  //     `https://api.spoonacular.com/recipes/guessNutrition?apiKey=${process.env.SPOONACULAR_API_KEY}&title=${queryTitle}`
+  //     `https://api.spoonacular.com/recipes/guessNutrition?apiKey=${process.env.SPOONACULAR_API_KEY}&title=${title}`
   //   );
   //   const recipeNutrition = await response.json();
   //   res.status(StatusCodes.OK).json({
--- server/routers/recipes.js
+++ server/routers/recipes.js
@@ -5,17 +5,28 @@ import {
   createRecipe,
   editRecipe,
   deleteRecipe,
+  getRecipeNutrition,
+  getUsersRecipes,
 } from "../controllers/recipes.js";
-import { validateRecipeInput } from "../middleware/validationMiddleware.js";
+import {
+  validateRecipeInput,
+  // validateId,
+} from "../middleware/validationMiddleware.js";
 
 const router = Router();
 
 router.route("/").get(getRecipes).post(validateRecipeInput, createRecipe);
+
+router.route("/userRecipes").get(getUsersRecipes);
+
+// router.route("/userRecipes").get(validateId, getUsersRecipes);
 
 router
   .route("/:id")
   .get(getRecipe)
   .put(validateRecipeInput, editRecipe)
   .delete(deleteRecipe);
+
+router.route("/:id/nutrition").get(getRecipeNutrition);
 
 export default router;
