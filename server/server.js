bf9ef006538d056ae53acbd5bd25310f8e92dcca
Cathy Dao <cathydao95@gmail.com>
2023-10-23 10:28:33 -0700

    create not found and error middleware

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 // custom imports
 // routers
-import recipeRouter from "./routes/recipes.js";
+import recipeRouter from "./routers/recipes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -19,6 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/v1/recipes", recipeRouter);
+
+// not found middleware (404 - triggered when request is made to a nonexistant route)
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+// error middleware (500 - occurs when there is an error durng the processing of a request)
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
 
 app.listen(PORT, () =>
   console.log(`Server running on Port http://localhost:${PORT}`)
