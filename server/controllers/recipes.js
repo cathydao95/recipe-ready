a69206afc342866584de215a673de1f2b2d78e1f
Cathy Dao <cathydao95@gmail.com>
2023-10-23 10:40:06 -0700

    remove try/catch blocks due to express-async-error

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,83 +1,93 @@
 import db from "../db/db-connection.js";
 import "dotenv/config";
+import { StatusCodes } from "http-status-codes";
+import { NotFoundError } from "../errors/customErrors.js";
 
 export const getRecipes = async (req, res) => {
-  try {
-    const { rows: recipes } = await db.query("SELECT * FROM recipes");
-    res.status(200).json({
-      status: "success",
-      results: recipes.length,
-      data: { recipes },
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error });
-  }
+  const { rows: recipes } = await db.query("SELECT * FROM recipes");
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    results: recipes.length,
+    data: { recipes },
+  });
 };
 
 export const getRecipe = async (req, res) => {
-  try {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const { rows: recipe } = await db.query(
-      "SELECT * FROM recipes WHERE id=$1",
-      [id]
-    );
-    res.status(200).json({
-      status: "success",
-      data: { recipe },
-    });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
+  const { rows: recipe } = await db.query("SELECT * FROM recipes WHERE id=$1", [
+    id,
+  ]);
+
+  if (recipe.length === 0) throw new NotFoundError(`no recipe with id ${id}`);
+
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    data: { recipe },
+  });
 };
 
 export const createRecipe = async (req, res) => {
-  try {
-    const {} = req.body;
+  const {
+    title,
+    ingredients,
+    instructions,
+    prep__time,
+    image_url,
+    public_recipe,
+  } = req.body;
 
-    const { rows: newRecipe } = await db.query(
-      "INSERT INTO recipes (title, ingredients, instructions, prep_time, image_url, user_id, public) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-      []
-    );
-    res.status(200).json({
-      status: "success",
-      data: { newRecipe },
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error });
-  }
+  // testing public (figure out how to default public)
+  const { rows: newRecipe } = await db.query(
+    "INSERT INTO recipes (title, ingredients, instructions, prep_time, image_url, public) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [title, ingredients, instructions, prep__time, image_url, public_recipe]
+  );
+
+  // private
+  // const { rows: newRecipe } = await db.query(
+  //   "INSERT INTO recipes (title, ingredients, instructions, prep_time, image_url, user_id, public) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+  //   []
+  // );
+  res.status(StatusCodes.CREATED).json({
+    status: "success",
+    data: { newRecipe },
+  });
 };
 
 export const editRecipe = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const {} = req.body;
+  const { id } = req.params;
+  const {
+    title,
+    ingredients,
+    instructions,
+    prep__time,
+    image_url,
+    public_recipe,
+  } = req.body;
 
-    const { rows: updatedRecipe } = await db.query(
-      "UPDATE recipes SET (title, ingredients, instructions, prep_time, image_url user_id, public) = ($1, $2, $3, $4, $5, $6, $7) WHERE id = $8 RETURNING *",
-      []
-    );
-    res.status(200).json({
-      status: "success",
-      data: { updatedRecipe },
-    });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
+  const { rows: updatedRecipe } = await db.query(
+    "UPDATE recipes SET (title, ingredients, instructions, prep_time, image_url, public) = ($1, $2, $3, $4, $5, $6) WHERE id = $7 RETURNING *",
+    [title, ingredients, instructions, prep__time, image_url, public_recipe, id]
+  );
+
+  if (updatedRecipe.length === 0)
+    throw new NotFoundError(`no recipe with id ${id}`);
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    data: { updatedRecipe },
+  });
 };
 
 export const deleteRecipe = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedRecipe = await db.query("DELETE FROM recipes WHERE id = $1", [
-      id,
-    ]);
-    res.status(200).json({
-      status: "success",
-    });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
+  const { id } = req.params;
+  const { rows: deletedRecipe } = await db.query(
+    "DELETE FROM recipes WHERE id = $1",
+    [id]
+  );
+  console.log(deletedRecipe);
+  if (deletedRecipe.length === 0)
+    throw new NotFoundError(`no recipe with id ${id}`);
+  res.status(StatusCodes.OK).json({
+    status: "success",
+  });
 };
