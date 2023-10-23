c077a1fbe1e428d796671abef7570c2a2a483294
Cathy Dao <cathydao95@gmail.com>
2023-10-23 10:44:58 -0700

    add login and register user validation

--- server/middleware/validationMiddleware.js
+++ server/middleware/validationMiddleware.js
@@ -1,5 +1,6 @@
 import { body, validationResult } from "express-validator";
 import { BadRequestError } from "../errors/customErrors.js";
+import db from "../db/db-connection.js";
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -17,10 +18,6 @@ const withValidationErrors = (validateValues) => {
   ];
 };
 
-export const validateTest = withValidationErrors([
-  [body("name").notEmpty().withMessage("name is required")],
-]);
-
 export const validateRecipeInput = withValidationErrors([
   body("title").notEmpty().withMessage("title is required"),
   body("ingredients").notEmpty().withMessage("ingredients is required"),
@@ -28,4 +25,27 @@ export const validateRecipeInput = withValidationErrors([
   body("prep_time").notEmpty().withMessage("prep_time is required"),
   body("image_url").notEmpty().withMessage("image_url is required"),
   // body("user_id").notEmpty().withMessage("user id is required"),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body("name").notEmpty().withMessage("name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format"),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 6 })
+    .withMessage("password must be at least 6 characters long"),
+]);
+
+export const validateLoginInput = withValidationErrors([
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format"),
+  body("password").notEmpty().withMessage("password is required"),
 ]);
