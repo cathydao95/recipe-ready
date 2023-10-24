65749295c37a4203681880418d4e67ada6238961
Cathy Dao <cathydao95@gmail.com>
2023-10-23 19:08:57 -0700

    import react router and create page layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,49 @@
-import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  HomeLayout,
+  DashboardLayout,
+  Landing,
+  Register,
+  Login,
+  Error,
+  Results,
+  SearchByIngredients,
+  Recipe,
+  Bookmarked,
+  Create,
+  UserSettings,
+} from "./pages";
+import SearchByName from "./pages/Search/SearchByName";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+        children: [
+          { index: true, element: <Results /> },
+          { path: "search-ingredients", element: <SearchByIngredients /> },
+          { path: "search-name", element: <SearchByName /> },
+          { path: "results", element: <Results /> },
+          { path: ":id", element: <Recipe /> },
+          { path: "bookmarked", element: <Bookmarked /> },
+          { path: "create", element: <Create /> },
+          { path: "settings", element: <UserSettings /> },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
