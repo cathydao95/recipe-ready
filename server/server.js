f39107815794f2cd5a6b64d1473ce548bd865549
Cathy Dao <cathydao95@gmail.com>
2023-10-23 19:04:26 -0700

    clean up files

--- server/server.js
+++ server/server.js
@@ -3,11 +3,6 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import "dotenv/config";
 import morgan from "morgan";
-import cookieParser from "cookie-parser";
-
-// custom imports
-// routers
-import recipeRouter from "./routes/recipes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -17,8 +12,6 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use("/api/v1/recipes", recipeRouter);
 
 app.listen(PORT, () =>
   console.log(`Server running on Port http://localhost:${PORT}`)
