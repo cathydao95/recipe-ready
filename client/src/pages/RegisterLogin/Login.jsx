929e2a79c09f97acb908c0ee8c8c2d915f493592
Cathy Dao <cathydao95@gmail.com>
2023-10-23 19:20:45 -0700

    Create Register and Login Pages

--- client/src/pages/RegisterLogin/Login.jsx
+++ client/src/pages/RegisterLogin/Login.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FormRow } from "../../components";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    setLoginInfo((prevInput) => {
+      return { ...prevInput, [e.target.name]: e.target.value };
+    });
+  };
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch("http://localhost:8080/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/JSON",
+        },
+        credentials: "include",
+        body: JSON.stringify(loginInfo),
+      });
+
+      if (response.ok) {
+        let { msg } = await response.json();
+        navigate("/recipes");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="pageWrapper">
+      <h1 className="title">Recipe Ready</h1>
+      <form className="form">
+        <FormRow type="email" name="email" handleInput={handleInput} />
+        <FormRow type="password" name="password" handleInput={handleInput} />
+        <div className="formLinkContainer">
+          <p className="formText">Not a member?</p>
+          <Link to="/register" className="formLink">
+            Register
+          </Link>
+        </div>
+        <button className="formBtn" type="submit" onClick={loginUser}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/RegisterLogin/Register.jsx
+++ client/src/pages/RegisterLogin/Register.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FormRow } from "../../components";
 
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const [registrationInfo, setRegistrationInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    setRegistrationInfo((prevInput) => {
+      return { ...prevInput, [e.target.name]: e.target.value };
+    });
+  };
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch("http://localhost:8080/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/JSON",
+        },
+        credentials: "include",
+        body: JSON.stringify(registrationInfo),
+      });
+
+      if (response.ok) {
+        let { msg } = await response.json();
+        navigate("/recipes");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="pageWrapper">
+      <h1 className="title">Recipe Ready</h1>
+      <form className="form">
+        <FormRow
+          type="text"
+          name="firstName"
+          labelText="first name"
+          handleInput={handleInput}
+        />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          handleInput={handleInput}
+        />
+        <FormRow type="email" name="email" handleInput={handleInput} />
+        <FormRow type="password" name="password" handleInput={handleInput} />
+        <div className="formLinkContainer">
+          <p className="formText">Already a member? </p>
+          <Link to="/login" className="formLink">
+            Login
+          </Link>
+        </div>
+
+        <button className="formBtn" type="submit" onClick={registerUser}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
