a5c58f49a22ccf5a35a90ba03f8c91c585e1c9b7
Cathy Dao <cathydao95@gmail.com>
2023-10-23 20:38:54 -0700

    Create Dashboard & import children/sidebar/navbar

--- client/src/pages/Layouts/DashboardLayout.jsx
+++ client/src/pages/Layouts/DashboardLayout.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import { useState, createContext, useContext } from "react";
+import { Sidebar, Navbar } from "../../components";
+
+const DashboardContext = createContext();
 
 const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+  const user = { name: "cathy" };
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    console.log("logout user");
+  };
+  return (
+    <DashboardContext.Provider
+      value={{ user, showSidebar, toggleSidebar, logoutUser }}
+    >
+      <div>
+        <Sidebar />
+        <div>
+          <Navbar />
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </DashboardContext.Provider>
+  );
 };
+
+export const useDashboardContext = () => useContext(DashboardContext);
 
 export default DashboardLayout;
