146a2513fda7036933707ed27c709a8e712a61f3
Cathy Dao <cathydao95@gmail.com>
2023-10-25 21:54:57 -0700

    Create Personal Recipes Page

--- client/src/pages/UserRecipes/UserRecipes.jsx
+++ client/src/pages/UserRecipes/UserRecipes.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Loading, ResultsLayout } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import { useLocation } from "react-router-dom";
 
 const UserRecipes = () => {
-  return <div>UserRecipes</div>;
+  const { isLoading, setIsLoading } = useAppContext();
+  const [usersRecipes, setUsersRecipes] = useState([]);
+
+  const getPersonalRecipes = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/v1/recipes/userRecipes",
+        {
+          credentials: "include",
+        }
+      );
+
+      if (response.ok) {
+        const {
+          data: { recipes },
+        } = await response.json();
+        setUsersRecipes(recipes);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(isLoading);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPersonalRecipes();
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <ResultsLayout recipes={usersRecipes} title="My Recipes" />
+  );
 };
 
 export default UserRecipes;
