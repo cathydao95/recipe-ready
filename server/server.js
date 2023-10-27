28d3f2605c55f601777186c22bf616d4c0c41360
Cathy Dao <cathydao95@gmail.com>
2023-10-26 18:31:31 -0700

    implement static files

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import "express-async-errors";
 import express, { urlencoded } from "express";
 import cors from "cors";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import "dotenv/config";
 import morgan from "morgan";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const REACT_BUILD_DIR = path.join(__dirname, "..", "client", "build");
 app.use(express.static(REACT_BUILD_DIR));
 
