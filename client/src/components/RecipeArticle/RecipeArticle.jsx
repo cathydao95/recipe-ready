1f6231016f18deaf363dcb183d4569987cd25f3b
Cathy Dao <cathydao95@gmail.com>
2023-10-26 21:03:53 -0700

    Create Receipe Article Component

---
+++ client/src/components/RecipeArticle/RecipeArticle.jsx
@@ -0,0 +1,23 @@
+import styles from "./styles.module.scss";
+import { FaRegClock, FaRegBookmark } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+const RecipeArticle = ({ recipe }) => {
+  const { id, title, image_url } = recipe;
+  return (
+    <NavLink to={`/dashboard/${id}`}>
+      <div className={styles.articleContainer}>
+        <div className={styles.imgContainer}>
+          <img className={styles.img} src={image_url} alt={title} />
+        </div>
+
+        <div>
+          <h5 className={styles.recipeTitle}>{title}</h5>
+        </div>
+      </div>
+    </NavLink>
+  );
+};
+
+export default RecipeArticle;
