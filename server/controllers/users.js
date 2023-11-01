cb68595f3eda636ac9bf460561a26e8087fd1e37
Cathy Dao <cathydao95@gmail.com>
2023-10-31 23:01:44 -0700

    import bcrypt to users controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,6 +2,7 @@ import db from "../db/db-connection.js";
 import "dotenv/config";
 import { StatusCodes } from "http-status-codes";
 import { NotFoundError } from "../errors/customErrors.js";
+import bcrypt from "bcrypt";
 
 // GET ALL USERS (FOR TESTING PURPOSES ON POSTMAN TO KEEP TRACK OF USER DB <-- WILL BE REMOVED)
 export const getUsers = async (req, res) => {
@@ -33,12 +34,12 @@ export const getUser = async (req, res) => {
 export const editUser = async (req, res) => {
   const currentUserId = req.user.userId;
   // incorporate password update
-  const { name, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
   const salt = bcrypt.genSaltSync(10);
   const hashed_password = bcrypt.hashSync(password, salt);
   const updatedUser = await db.query(
-    "UPDATE users SET (name, email, hashed_password) = ($1, $2, $3) WHERE id = $4",
-    [name, email, hashed_password, currentUserId]
+    "UPDATE users SET (first_name, last_name, email, hashed_password) = ($1, $2, $3, $4) WHERE id = $5",
+    [firstName, lastName, email, hashed_password, currentUserId]
   );
 
   res.status(StatusCodes.OK).json({
