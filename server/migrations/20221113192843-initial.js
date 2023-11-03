3698b07031d13e4e585b92b2d3556e81adc05441
Cathy Dao <cathydao95@gmail.com>
2023-11-03 12:56:56 -0700

    revert migration files

--- server/migrations/20221113192843-initial.js
+++ server/migrations/20221113192843-initial.js
@@ -1,58 +1,59 @@
+"use strict";
+
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-let dbm;
-let type;
-let seed;
-let Promise;
+var dbm;
+var type;
+var seed;
+var Promise;
 
-// setup function
-export function setup(options, seedLink) {
+/**
+ * Receive the dbmigrate dependency from dbmigrate initially.
+ * This enables me to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
   Promise = options.Promise;
-}
+};
 
-export function up(db) {
-  const filePath = path.join(
-    __dirname,
-    "sqls",
-    "20221113192843-initial-up.sql"
-  );
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+exports.up = function (db) {
+  var filePath = path.join(__dirname, "sqls", "20221113192843-initial-up.sql");
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
       if (err) return reject(err);
       console.log("received data: " + data);
+
       resolve(data);
     });
-  }).then((data) => {
+  }).then(function (data) {
     return db.runSql(data);
   });
-}
+};
 
-export function down(db) {
-  const filePath = path.join(
+exports.down = function (db) {
+  var filePath = path.join(
     __dirname,
     "sqls",
     "20221113192843-initial-down.sql"
   );
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
       if (err) return reject(err);
       console.log("received data: " + data);
+
       resolve(data);
     });
-  }).then((data) => {
+  }).then(function (data) {
     return db.runSql(data);
   });
-}
+};
 
-// _meta export
-export const _meta = {
+exports._meta = {
   version: 1,
 };
