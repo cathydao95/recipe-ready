0ce124376d58105009960da6d85ed29699bf6cce
Cathy Dao <cathydao95@gmail.com>
2023-11-03 22:49:15 -0700

    remove unused code

--- client/src/components/ResultsLayout/ResultsLayout.jsx
+++ client/src/components/ResultsLayout/ResultsLayout.jsx
@@ -6,24 +6,11 @@ import EmptyPageContent from "../EmptyPageContent/EmptyPageContent";
 
 const ResultsLayout = ({ recipes, title, page, isLoading }) => {
   return (
-    // <div className={clsx("wrapper")}>
-    //   <h2 className={styles.title}>{title}</h2>
-    //   {isLoading ? (
-    //     <Loading />
-    //   ) : recipes.length === 0 ? (
-    //     <EmptyPageContent page={page} />
-    //   ) : (
-    //     <div className={styles.recipesContainer}>
-    //       {recipes.map((recipe) => (
-    //         <RecipeCard key={recipe.id} recipe={recipe} />
-    //       ))}
-    //     </div>
-    //   )}
-    // </div>
     <div className={clsx("wrapper")}>
       <h2 className={styles.title}>{title}</h2>
-
-      {recipes.length === 0 ? (
+      {isLoading ? (
+        <Loading />
+      ) : recipes.length === 0 ? (
         <EmptyPageContent page={page} />
       ) : (
         <div className={styles.recipesContainer}>
