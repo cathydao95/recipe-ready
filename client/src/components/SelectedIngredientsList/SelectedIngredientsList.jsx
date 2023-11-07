ec310149e6e7fdc91a143c8f7c774a65643664c9
Cathy Dao <cathydao95@gmail.com>
2023-11-06 19:01:27 -0800

    create component to display selected ingredients

---
+++ client/src/components/SelectedIngredientsList/SelectedIngredientsList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import { FaRegCheckSquare } from "react-icons/fa";
+
+const SelectedIngredientsList = ({
+  selectedIngredients,
+  setSelectedIngredients,
+}) => {
+  const handleIngredientClick = (ingredientToRemove) => {
+    const updatedIngredients = selectedIngredients.filter(
+      (ing) => ing !== ingredientToRemove
+    );
+    setSelectedIngredients(updatedIngredients);
+  };
+  return (
+    <div className={styles.selectedContainer}>
+      {selectedIngredients.map((ing) => (
+        <div
+          key={ing}
+          className={styles.selectedIng}
+          onClick={() => handleIngredientClick(ing)}
+        >
+          <FaRegCheckSquare className={styles.icon} />
+          {ing}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SelectedIngredientsList;
