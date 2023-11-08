9e55ae4a4f65e953b7d9b9c82a3c569d8f724689
Cathy Dao <cathydao95@gmail.com>
2023-11-07 22:04:34 -0800

    update naming convention of color

--- client/src/components/SelectedIngredientsList/SelectedIngredientsList.jsx
+++ client/src/components/SelectedIngredientsList/SelectedIngredientsList.jsx
@@ -13,20 +13,20 @@ const SelectedIngredientsList = ({
     setSelectedIngredients(updatedIngredients);
   };
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.selectedContainer}>
-        {selectedIngredients.map((ing) => (
-          <div
-            key={ing}
-            className={styles.selectedIng}
-            onClick={() => handleIngredientClick(ing)}
-          >
-            <FaRegCheckSquare className={styles.icon} />
-            {ing}
-          </div>
-        ))}
-      </div>
+    // <div className={styles.wrapper}>
+    <div className={styles.selectedContainer}>
+      {selectedIngredients.map((ing) => (
+        <div
+          key={ing}
+          className={styles.selectedIng}
+          onClick={() => handleIngredientClick(ing)}
+        >
+          <FaRegCheckSquare className={styles.icon} />
+          {ing}
+        </div>
+      ))}
     </div>
+    // </div>
   );
 };
 
