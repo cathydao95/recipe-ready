7722d4855b517b61d0e648495d6b9c3a01202832
Cathy Dao <cathydao95@gmail.com>
2023-11-10 10:24:46 -0800

    center title and move back button to front

--- client/src/components/ResultsLayout/ResultsLayout.jsx
+++ client/src/components/ResultsLayout/ResultsLayout.jsx
@@ -3,13 +3,30 @@ import clsx from "clsx";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import EmptyPageContent from "../EmptyPageContent/EmptyPageContent";
 import { useAppContext } from "../../context/appContext";
+import { useNavigate } from "react-router-dom";
+import { BsBackspace } from "react-icons/bs";
 
 const ResultsLayout = ({ recipes, title, page }) => {
   const { resultsLoaded } = useAppContext();
+  const navigate = useNavigate();
   return (
     resultsLoaded && (
       <div className={clsx("wrapper")}>
-        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.headerContainer}>
+          {recipes && recipes.length > 0 && (
+            <button
+              onClick={() => {
+                navigate(-1);
+              }}
+              className={styles.backBtn}
+            >
+              <BsBackspace />
+            </button>
+          )}
+          <h1 className={styles.title}>{title}</h1>
+          {/* Create an invisible spacer element so that title can be centered and back button can be at the start */}
+          <div className={styles.spacer}></div>
+        </div>
         {!recipes || recipes.length === 0 ? (
           <EmptyPageContent page={page} />
         ) : (
