e3d3c678890fb514be399ad1774687e87b333695
Cathy Dao <cathydao95@gmail.com>
2023-11-11 17:45:47 -0800

    add nutrition info error handing and update css

--- client/src/components/NutritionTable/NutritionTable.jsx
+++ client/src/components/NutritionTable/NutritionTable.jsx
@@ -11,32 +11,41 @@ const NutritionTable = ({ recipeNutrition }) => {
             <th>Amount</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Calories</td>
-            <td>
-              {recipeNutrition.calories?.value} {recipeNutrition.calories?.unit}
-            </td>
-          </tr>
-          <tr>
-            <td>Carbs</td>
-            <td>
-              {recipeNutrition.carbs?.value} {recipeNutrition.carbs?.unit}
-            </td>
-          </tr>
-          <tr>
-            <td>Protein</td>
-            <td>
-              {recipeNutrition.protein?.value} {recipeNutrition.protein?.unit}
-            </td>
-          </tr>
-          <tr>
-            <td>Fat</td>
-            <td>
-              {recipeNutrition.fat?.value} {recipeNutrition.fat?.unit}
-            </td>
-          </tr>
-        </tbody>
+        {recipeNutrition.status === "error" ? (
+          <tbody>
+            <tr>
+              <td colSpan="2">Not enough data for an informed guess</td>
+            </tr>
+          </tbody>
+        ) : (
+          <tbody>
+            <tr>
+              <td>Calories</td>
+              <td>
+                {recipeNutrition.calories?.value}{" "}
+                {recipeNutrition.calories?.unit}
+              </td>
+            </tr>
+            <tr>
+              <td>Carbs</td>
+              <td>
+                {recipeNutrition.carbs?.value} {recipeNutrition.carbs?.unit}
+              </td>
+            </tr>
+            <tr>
+              <td>Protein</td>
+              <td>
+                {recipeNutrition.protein?.value} {recipeNutrition.protein?.unit}
+              </td>
+            </tr>
+            <tr>
+              <td>Fat</td>
+              <td>
+                {recipeNutrition.fat?.value} {recipeNutrition.fat?.unit}
+              </td>
+            </tr>
+          </tbody>
+        )}
       </table>
     </div>
   );
