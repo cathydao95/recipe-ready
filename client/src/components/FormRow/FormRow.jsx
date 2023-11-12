a12df5cb8af479934ed5725f3ec2c79273e6c906
Cathy Dao <cathydao95@gmail.com>
2023-11-11 19:04:30 -0800

    convert instructions text input to text area

--- client/src/components/FormRow/FormRow.jsx
+++ client/src/components/FormRow/FormRow.jsx
@@ -1,6 +1,20 @@
 // Reusable input for login/register/create/edit forms
 const FormRow = ({ type, name, labelText, handleInput, value }) => {
-  return (
+  return type === "textarea" ? (
+    <div>
+      <textarea
+        className="formTextArea"
+        id={name}
+        name={name}
+        autoComplete="off"
+        placeholder={labelText || name}
+        required
+        onChange={handleInput}
+        value={value}
+        rows={7}
+      ></textarea>
+    </div>
+  ) : (
     <div className="formRow">
       <input
         className="formInput"
